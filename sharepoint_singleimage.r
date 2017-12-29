db9d9ab756b70445b9b489495af449ca5c460754
Armağan Gözütok <armagango@gmail.com>
2017-12-29 10:29:31 +0300

    Update sharepoint_singleimage.r

--- sharepoint_singleimage.r
+++ sharepoint_singleimage.r
@@ -2,14 +2,19 @@
 
 require(httr)
 
-url <- "https://sharepoint_or_something.company_name.com/dir/dir/dir/username/Shared%20Documents/some_more_directory/20171201.png"
-r <- GET(url, authenticate("username","password",type="any"), verbose())
+url <- "https://sharepoint_or_something.company_name.com/dir/dir/dir/username/Shared%20Documents/some_more_directory/20171201.png" # change this too
+username <- "change_this"
+password <- "change_this"
+
+
+r <- GET(url, authenticate(username,password,type="any"), verbose())
 
 atmos <- content(r, "parsed")
 
-dim(atmos)
+dim(atmos) # dimensions of an image
+
 library(grid)
-grid.raster(atmos)
+grid.raster(atmos) # plot in true colors
 
 
 
