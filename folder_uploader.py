7921e251231e0203744fde81754eab7f8f7cdd04
Armağan Gözütok <armagango@gmail.com>
2018-02-15 17:10:25 +0300

    Update folder_uploader.py

--- folder_uploader.py
+++ folder_uploader.py
@@ -8,16 +8,16 @@ path = 'path to directory including files or documents to upload'
 
     
 files = next(os.walk(path))[2]
-print "File array:"
-print files
+print "File array:" 
+print files #displays file array
 print "\n"
-print 'Total number of files in', path, 'is', len(files)
+print 'Total number of files in', path, 'is', len(files) #evaluates total number of documents to upload
 print "\n"
 print "Files listed in directory:"
 print "\n"
 i = 0
 for i in range(0,len(files)):
-    print(path+"\\."+files[i])
+    print(path+"\\."+files[i]) #lists the files with correct paths
     i += 1
 print "\n"
    
@@ -48,7 +48,7 @@ for i in range(0, len(files)):
     
     resp = requests.put('Full directory path including hostname where the files will be uploaded' + filename, data=bytes, auth=session.auth)
             
-    print "Status response for file #",i+1, "is", resp.status_code
+    print "Status response for file #",i+1, "is", resp.status_code #prints out status code of current iteration in request loop
 
 
 
